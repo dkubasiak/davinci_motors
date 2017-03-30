5925243c0ad41e9d50f92658b96c1898b098e66d
Dave Kubasiak <dkubasiak@comverge.com>
2017-03-30 09:12:12 -0400

    Added test for second car

--- spec/features/creating_cars_spec.rb
+++ spec/features/creating_cars_spec.rb
@@ -15,4 +15,19 @@ feature 'Creating Cars' do
 
     expect(page).to have_content('1967 Ford Mustang $2300.0 created')
   end
+
+  scenario 'can create a second car' do
+    visit '/'
+    
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Toyota'
+    fill_in 'Model', with: 'Camry'
+    fill_in 'Year', with: '2010'
+    fill_in 'Price', with: '5400'
+
+    click_button 'Create Car'
+
+    expect(page).to have_content('2010 Toyota Camry $5400.0 created')
+  end
 end
